18d29fc51b5c644a3ddff8ccf3d711f8615b3992
noamgot <noam.got+github@gmail.com>
2017-06-01 09:32:52 +0300

    my work so far...

---
+++ hw3/Battleship3D/GameResultsTable.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <condition_variable>
+#include <vector>
+#include "BattleshipTournament.h"
+
+// todo - consider being friend class with BattleshipTournament
+
+class GameResultsTable
+{
+public:
+	GameResultsTable() = delete;
+	GameResultsTable(int numPlayers, int numRounds) : _numPlayers(numPlayers), _numRounds(numRounds) { _table.resize(numRounds); }
+	
+
+	// block copy & move ctors and assignments
+	GameResultsTable(const GameResultsTable& other) = delete;
+	GameResultsTable& operator=(const GameResultsTable& other) = delete;
+	GameResultsTable(GameResultsTable&& other) noexcept = delete;
+	GameResultsTable& operator=(GameResultsTable&& other) noexcept = delete;
+
+	/* the wanted round results*/
+	const std::vector<PlayerGameResults>& getRoundResults(int round);
+	void updateTable(int round, PlayerGameResults gameResults);
+
+private:
+	std::vector<std::vector<PlayerGameResults>> _table;
+	std::mutex _mutex;
+	std::condition_variable _cv;
+	int _numPlayers;
+	int _numRounds;
+};
--- hw3/Battleship3D/MyBoardData.h
+++ hw3/Battleship3D/MyBoardData.h
@@ -1,16 +1,19 @@
 ﻿#pragma once
 #include "IBattleshipGameAlgo.h"
 #include <vector>
+#include <memory>
 
 class MyBoardData : public BoardData
 {
 public:
-	MyBoardData(int rows, int cols, int depth);
+	MyBoardData(int rows, int cols, int depth, int playerNum, std::vector<std::vector<std::vector<char>>> * pBoard);
 
 	char charAt(Coordinate c) const override { return _board[c.row][c.col][c.depth]; }
 	void setChar(Coordinate coor, char ch) { _board[coor.row][coor.col][coor.depth] = ch; }
 
 private:
+	int _playerNum;
+	std::unique_ptr<std::vector<std::vector<std::vector<char>>>> _pBoard;
 	std::vector<std::vector<std::vector<char>>> _board; // 3D board!
 	
 };
