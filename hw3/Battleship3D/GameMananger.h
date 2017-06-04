b32fdd94ef6f4abf3dd3523650cc18199ef97451
noamgot <noam.got+github@gmail.com>
2017-06-04 21:59:50 +0300

    game manager now has a logger object

--- hw3/Battleship3D/GameMananger.h
+++ hw3/Battleship3D/GameMananger.h
@@ -2,12 +2,13 @@
 #include "CompetitionManager.h"
 #include "IBattleshipGameAlgo.h"
 #include "MyBoardData.h"
+#include "Logger.h"
 
 class GameMananger
 {
 public:
 	GameMananger() = delete;
-	explicit GameMananger(Game game) : _grA(game.idA), _grB(game.idB) {}
+	explicit GameMananger(Game game, std::shared_ptr<Logger> pLogger) : _grA(game.idA), _grB(game.idB), _pLogger(pLogger) {}
 	int runGame();
 
 	void initIndividualBoards(MyBoardData& boardA, MyBoardData& boardB) const;
@@ -19,7 +20,7 @@ private:
 	std::unique_ptr<IBattleshipGameAlgo> _A;
 	std::unique_ptr<IBattleshipGameAlgo> _B;
 	MyBoardData _boardData;
-
 	PlayerGameResults _grA;
 	PlayerGameResults _grB;
+	std::shared_ptr<Logger> _pLogger;
 };
