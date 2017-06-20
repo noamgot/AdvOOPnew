609aee9e26802f1b676c421e1bf4f10711841b6d
bennzo <bennohay@gmail.com>
2017-06-20 23:15:22 +0300

    ben's test

--- hw4/hw4/Matrix2d.cpp
+++ hw4/hw4/Matrix2d.cpp
@@ -1,1 +1,17 @@
 ﻿#include "Matrix2d.h"
+
+template <typename T>
+using deref_iter_t = std::remove_reference_t<decltype(*std::declval<T>())>;
+
+template<class Iterator, class GroupingFunc>
+auto groupValues(Iterator begin, Iterator end, GroupingFunc groupingFunc)
+{
+	using T = deref_iter_t<Iterator>;
+	using GroupingType = std::result_of_t<GroupingFunc(T&)>;
+	std::map<GroupingType, std::list<Coord2d>> groups;
+	std::for_each(begin, end, [&groups, groupingFunc](const auto& val) {
+		groups[groupingFunc(val)].push_back(Coord2d());
+	});
+	
+	return groups;
+}
